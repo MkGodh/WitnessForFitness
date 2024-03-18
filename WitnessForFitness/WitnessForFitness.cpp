aff55b65aa84ce5c89d991d83c1c1654a5c249ff
MkGodh <fenryrvla@yandex.ru>
2024-03-18 15:24:37 +0300

    Добавьте файлы проекта.

---
+++ WitnessForFitness/WitnessForFitness.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Rus");
+    using namespace std;
+
+    float protein;
+    float carbohydrate;
+    float weightNetto;
+
+    cout << "Здравствуйте Лена! Это программа похудейка.\nВведите пожалуйст колличество белков на 100 грамм продукта: ";
+    cin >> protein;
+    cout << "А теперь введит колличество углеводов на 100 грамм продукта: ";
+    cin >> carbohydrate;
+    cout << "А теперь введите общую массу нетто: ";
+    cin >> weightNetto;
+
+    cout << "В этом продукте содержится " << weightNetto / 100 * protein << " грамм белка, а также " << weightNetto / 100 * carbohydrate << " грамм углеводов!";
+}
